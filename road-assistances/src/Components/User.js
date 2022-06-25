7e55345faa386d48c10e4f58002362882a59af73
Abishek Kumar <87654898+ABISHEK-KU@users.noreply.github.com>
2022-06-25 14:19:09 +0000

    router

---
+++ road-assistances/src/Components/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/User.css';
+const User=(props)=>{
+
+    return(
+        <div>
+            <h1 className='heading'>ABC Assitance</h1>
+        </div>
+    )
+}
+export default User
+
